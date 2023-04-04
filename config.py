4bb3ed214248f27cc6d547e93f354354b59d8d47
anisagar123 <65108383+anisagar123@users.noreply.github.com>
2023-04-04 19:46:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
-import os
+import re, os
 
-class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5800078311:AAFeq-7UDwnq8KiZEJOa-YHT228jgYRMCk4")
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "bb8f36f9c39a24c7f8b2acbc7ea8c60a")
-    DB_URL = os.environ.get("DATABASE_URL", "")
+id_pattern = re.compile(r'^.\d+$') 
+
+API_ID = os.environ.get("API_ID", "15671595")
+
+API_HASH = os.environ.get("API_HASH", "bb8f36f9c39a24c7f8b2acbc7ea8c60a")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6144136776:AAHX7p92ajDcQj_WdXytpyDt6sRr-twbGrg") 
+
+FORCE_SUB = os.environ.get("FORCE_SUB", "TG_UPDATES1")
+
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+
+PORT = os.environ.get('PORT', '8080')
