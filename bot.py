de96842ce351de047049a178e91c6c376c1ff67b
anisagar123 <65108383+anisagar123@users.noreply.github.com>
2023-04-04 19:30:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,46 @@
-import pyrogram
-
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-import os
-
-from config import Config
 from pyrogram import Client 
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
+from config import API_ID, API_HASH, BOT_TOKEN, FORCE_SUB, PORT
+from aiohttp import web
+from route import web_server
 
-class autocaption(Client):
-    
+class Bot(Client):
+
     def __init__(self):
         super().__init__(
-            session_name="Captioner",
-            bot_token = Config.BOT_TOKEN,
-            api_id = Config.API_ID,
-            api_hash = Config.API_HASH,
-            workers = 20,
-            plugins = dict(
-                root="plugins"
-            )
+            name="renamer",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=200,
+            plugins={"root": "plugins"},
+            sleep_threshold=15,
         )
 
-if __name__ == "__main__" :
-    autocaption().run()
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        self.mention = me.mention
+        self.username = me.username 
+        self.force_channel = FORCE_SUB
+        if FORCE_SUB:
+            try:
+                link = await self.export_chat_invite_link(FORCE_SUB)                  
+                self.invitelink = link
+            except Exception as e:
+                print(e)
+                print("Make Sure Bot admin in force sub channel")             
+                self.force_channel = None
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"       
+        await web.TCPSite(app, bind_address, PORT).start()     
+        print(f"{me.first_name} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳 ⚡️⚡️⚡️")
+      
+
+    async def stop(self, *args):
+        await super().stop()      
+        print("Bot Stopped")
+       
+
+bot=Bot()
+bot.run()
