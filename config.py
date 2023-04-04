3cd0dbc3db502ca1f9a55fb509e9f7e8efacb9d8
anisagar123 <65108383+anisagar123@users.noreply.github.com>
2023-04-04 19:55:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,4 @@ API_ID = os.environ.get("API_ID", "15671595")
 
 API_HASH = os.environ.get("API_HASH", "bb8f36f9c39a24c7f8b2acbc7ea8c60a")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6144136776:AAHX7p92ajDcQj_WdXytpyDt6sRr-twbGrg") 
-
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6144136776:AAHX7p92ajDcQj_WdXytpyDt6sRr-twbGrg")
