16e03309789c66f9a23d30558a9abb2f30f142e1
anisagar123 <65108383+anisagar123@users.noreply.github.com>
2023-04-04 19:48:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,4 @@ API_HASH = os.environ.get("API_HASH", "bb8f36f9c39a24c7f8b2acbc7ea8c60a")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6144136776:AAHX7p92ajDcQj_WdXytpyDt6sRr-twbGrg") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "TG_UPDATES1")
-
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
-
-PORT = os.environ.get('PORT', '8080')
