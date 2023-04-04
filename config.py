2c0e0958d6a02b5f8b9aee0c7766c790d943c9bd
anisagar123 <65108383+anisagar123@users.noreply.github.com>
2023-04-04 18:45:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5800078311:AAFeq-7UDwnq8KiZEJOa-YHT228jgYRMCk4")
     API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "bb8f36f9c39a24c7f8b2acbc7ea8c60a")
     DB_URL = os.environ.get("DATABASE_URL", "")
